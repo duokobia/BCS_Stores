b941e3c8d7c5f195977a045729b5697df188b253
Daniel Okobia <duokobia@gmail.com>
2023-07-13 00:16:48 +0100

    Initial Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+import Header from "./components/header";
+import Main from "./components/main";
+import Footer from "./components/footer";
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <Main />
+      <Footer />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ client/src/components/header/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <h2>BCS STORES</h2>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">New In</a>
+          </li>
+          <li>
+            <a href="/">Beddings</a>
+          </li>
+          <li>
+            <a href="/">Clothings</a>
+          </li>
+          <li>
+            <a href="/">Stores</a>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <span>icon</span>
+        <span>icon</span>
+        <span>icon</span>
+        <span>icon</span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
